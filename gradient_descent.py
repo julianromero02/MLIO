eb2a1e0232392f909e46098eb17d05c3ccb55ca0
julianromero02 <jdarioromeror@javeriana.edu.co>
2024-07-02 17:12:35 -0400

    Logit & GD

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,28 +1,40 @@
 import numpy as np
 
-def batch_gradient_descent(X, y, learning_rate=0.01, n_iter=1000):
+def batch_gradient_descent(X, y, learning_rate=0.01, n_iter=1000, regression_type="linear_regression"):
     n_samples, n_features = X.shape
     weights = np.zeros(n_features)
     for _ in range(n_iter):
         # Compute the gradients
-        gradients = 2/np.dot(X.T, X.dot(weights) - y) / n_samples
+        if regression_type == "linear_regression":
+            
+            gradients = 2/np.dot(X.T, X.dot(weights) - y) / n_samples
+        elif regression_type == "logistic":
+            gradients = (sigmod(weights.T.dot(X)) - y) / n_samples
         # Update the weights
         weights -= learning_rate * gradients
     return weights
 def learning_rate_schedule(initial_learning_rate, t, decay=0.1):
     return initial_learning_rate / (1 + decay * t)
-def stochastic_gradient_descent(X, y, learning_rate=0.01, n_epochs=1000):
+def stochastic_gradient_descent(X, y, learning_rate=0.01, n_iter=1000, regression_type='linear_regression'):
     "It is faster and takes only one instance of the batch"
     n_samples, n_features = X.shape
     weights = np.zeros(n_features)
     random_index=np.random.randint(n_samples)
-    for epoch in range(n_epochs):
+    for epoch in range(n_iter):
         for i in range(n_samples):
             random_index = np.random.randint(n_samples)
             xi = X[random_index:random_index + 1]
             yi=y[random_index:random_index + 1]
             # Compute the gradient for the i-th sample
-            gradient = 2/ np.dot(xi.T, xi.dot(weights) - yi) / n_samples
+            if regression_type == 'logistic':
+                gradient=(sigmod(weights.T.dot(xi))-yi).dot(xi)
+            if regression_type == 'linear_regression':
+                gradient = 2/ np.dot(xi.T, xi.dot(weights) - yi) / n_samples #Cambiar para aceptar los gradientes de diferentes funciones
             learning_rate = learning_rate_schedule(learning_rate,epoch )
             # Update the weights
             weights -= learning_rate * gradient
+    return weights
+
+
+def sigmod(z):
+    return 1 / (1 + np.exp(-z))
--- simple_lineal_regression.py
+++ simple_lineal_regression.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+from gradient_descent import *
 
 x=np.random.randint(1,100,100)
 y= x**2+100
@@ -26,7 +26,13 @@ b = mean(df['y']) - a*mean(df['x'])
 def predict_SL(valor,a,b):
     return a*valor+b
 valor=int(input("Ingrese el area de su casa"))
-precio=predict_SL(valor,a,b)
+linear_mse=predict_SL(valor,a,b)
+gradient = stochastic_gradient_descent(df[['x']].values,df['y'].values,learning_rate=0.01,n_iter=1000,regression_type='linear_regression')
+linear_gd= gradient*valor
 
-print("El precio de su casa es: ",precio)
+
+print("El precio de su casa es: ",linear_mse)
+
+
+print("El precio de su casa con Gradient Descent es: ",linear_gd)
 print("A")
