fba2097f7c862ca906b2d549a3ef1da09655bf85
Juan Soto <soto97@purdue.edu>
2024-07-04 02:29:27 -0400

    Tuned parameters, solved problems on equations

--- simple_lineal_regression.py
+++ simple_lineal_regression.py
@@ -2,37 +2,42 @@ import numpy as np
 import pandas as pd
 from gradient_descent import *
 
-x=np.random.randint(1,100,100)
-y= x**2+100
 
-df=pd.DataFrame({'x':x,'y':y})
 def mean(x):
     return sum(x)/len(x)
 
 def snn(df,x,y):
-    x_mean=mean(df[x])
-    y_mean=mean(df[y])
-    x_vector=df['x'].values
-    y_vector=df['y'].values
+    x_mean=np.mean(df[x].values)
+    y_mean=np.mean(df[y].values)
+    x_vector=df[x].values
+    y_vector=df[y].values
     xsubs = x_vector-x_mean
     sxx=np.sum(xsubs)**2
     sxy=np.sum((y_vector-y_mean)) * np.sum(xsubs)
     return sxx, sxy
 
-sxx,sxy = snn(df,'x','y')
-a = sxy/sxx
-b = mean(df['y']) - a*mean(df['x'])
-
 def predict_SL(valor,a,b):
     return a*valor+b
+
+
+from numpy.random import MT19937
+from numpy.random import RandomState, SeedSequence
+rs = RandomState(MT19937(SeedSequence(123456789)))
+
+x=np.random.randint(60,500,500)
+y= 4*x+1200
+
+df=pd.DataFrame({'x':x,'y':y})
+sxx,sxy = snn(df,'x','y')
+a = np.divide(sxy,sxx)
+b = np.mean(df['y'].values) - a*np.mean(df['x'].values)
+
 valor=int(input("Ingrese el area de su casa"))
 linear_mse=predict_SL(valor,a,b)
-gradient = stochastic_gradient_descent(df[['x']].values,df['y'].values,learning_rate=0.01,n_iter=1000,regression_type='linear_regression')
-linear_gd= gradient*valor
+weights = batch_gradient_descent(df['x'].values,df['y'].values,learning_rate=0.00001,n_iter=1700000,regression_type='linear_regression')
+linear_gd= weights[0]+weights[1]*valor
 
 
 print("El precio de su casa es: ",linear_mse)
-
-
 print("El precio de su casa con Gradient Descent es: ",linear_gd)
-print("A")
+print(weights)
