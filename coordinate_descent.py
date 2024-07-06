0527b3e65e01f10805f139490ccb74301ea30256
Juan Soto <soto97@purdue.edu>
2024-07-06 16:51:58 -0400

    Solved dimensional dot product

--- coordinate_descent.py
+++ coordinate_descent.py
@@ -1,22 +1,20 @@
 import numpy as np
+from gradient_descent import extracting_initialinfo
 
 def coordinate_descent(X,y,learning_rate=0.01, n_iter=1000, regression_type="linear_regression"):
     
     #Coordinate descent iterates per weight and take the other as constants
 
-    ones = np.ones(X.shape[0])
-    x_n = np.vstack((ones,X)) ## Intercept
-    n_samples, n_features = x_n.T.shape
-    weights = np.zeros(n_features)
+    x_n, n_samples, n_features, weights = extracting_initialinfo(X)
 
     for _ in range(n_iter):
         for i in range(n_features):
 
-            xi = x_n[i,:].T
+            xi = x_n[:,i]
             
             if regression_type == "linear_regression":
 
-                gradient = (2/n_samples)*np.dot(xi,(np.dot(weights,x_n)-y))
+                gradient = (2/n_samples)*np.dot(xi,(np.dot(weights,x_n.T)-y))
             
                 weights[i] -= learning_rate * gradient
     return weights
